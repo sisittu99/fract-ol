9b707c4c525e978813e7d0bf7a95d1634c49281e
Mattia Cerchi <mcerchi@e3r10p4.42roma.it>
2022-03-15 21:18:48 +0100

    ciaone

--- fractol.h
+++ fractol.h
@@ -6,7 +6,7 @@
 /*   By: mcerchi <mcerchi@student.42roma.it>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/15 15:37:38 by mcerchi           #+#    #+#             */
-/*   Updated: 2022/03/15 15:41:47 by mcerchi          ###   ########.fr       */
+/*   Updated: 2022/03/15 17:41:02 by mcerchi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,12 @@
 
 # include "./math_lib/math_lib.h"
 # include "./mlx/mlx.h"
+
+typedef struct s_pxl
+{
+	int	r;
+	int	g;
+	int	b;
+}	t_pxl;
 
 #endif
