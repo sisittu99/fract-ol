93ed94555bc7b9f8a0e2d2993d05cec2413c8509
Mattia Cerchi <92301111+sisittu99@users.noreply.github.com>
2022-03-20 20:17:32 +0100

    zoom completato (?)

--- commands.c
+++ commands.c
@@ -29,34 +29,40 @@
 // 	mlx_loop(e->mlx.mlx);
 // }
 
-void	zoom_plus(int x, int y, t_env *e)
+void	ft_zoom(int x, int y, t_env *e, int isplus)
 {
 	t_cpx	virt_pos;
 	double	zoom_fact;
-	
-	zoom_fact = 0.95;
-	//creiamo il delta al quale poi applicare il fattore zoom
-	virt_pos = ft_sub_cpx(e->mlx.virt_max, e->mlx.virt_min);
-	virt_pos.x = x * virt_pos.x / WIDTH;
-	virt_pos.y = y * virt_pos.y / HEIGHT;
-	virt_pos = ft_add_cpx(virt_pos, e->mlx.virt_min);
-	e->mlx.zoom *= zoom_fact;
-	//ora lo applichiamo a virt_max&min
-	e->mlx.virt_max.x = e->mlx.virt_max.x * zoom_fact + virt_pos.x * (1 - zoom_fact);
-	e->mlx.virt_max.y = e->mlx.virt_max.y * zoom_fact + virt_pos.y * (1 - zoom_fact);
-	e->mlx.virt_min.x = e->mlx.virt_min.x * zoom_fact + virt_pos.x * (1 - zoom_fact);
-	e->mlx.virt_min.y = e->mlx.virt_min.y * zoom_fact + virt_pos.y * (1 - zoom_fact);
-	//piccolo spieghino reminder:
-	//virt_max è stato modificato secondo lo zoom factor, il che basterebbe per zoomare.
-	//Tuttavia, per permettere lo zoom nella posizione del mouse (dettata dalle coordinate e ricalcolate in virt_pos)
+	if (isplus == 1)
+		zoom_fact = 0.95;
+	else
+		zoom_fact = 1.05;
+	if (e->mlx.zoom <= 2.0)
+	{
+		//creiamo il delta al quale poi applicare il fattore zoom
+		virt_pos = ft_sub_cpx(e->mlx.virt_max, e->mlx.virt_min);
+		virt_pos.x = x * virt_pos.x / WIDTH;
+		virt_pos.y = y * virt_pos.y / HEIGHT;
+		virt_pos = ft_add_cpx(virt_pos, e->mlx.virt_min);
+		e->mlx.zoom *= zoom_fact;
+		//ora lo applichiamo a virt_max&min
+		e->mlx.virt_max.x = e->mlx.virt_max.x * zoom_fact + virt_pos.x * (1 - zoom_fact);
+		e->mlx.virt_max.y = e->mlx.virt_max.y * zoom_fact + virt_pos.y * (1 - zoom_fact);
+		e->mlx.virt_min.x = e->mlx.virt_min.x * zoom_fact + virt_pos.x * (1 - zoom_fact);
+		e->mlx.virt_min.y = e->mlx.virt_min.y * zoom_fact + virt_pos.y * (1 - zoom_fact);
+		//piccolo spieghino reminder:
+		//virt_max è stato modificato secondo lo zoom factor, il che basterebbe per zoomare.
+		//Tuttavia, per permettere lo zoom nella posizione del mouse (dettata dalle coordinate e ricalcolate in virt_pos)
+		//bisogna trovare un modo per rimanere nel punto zoomato. Questo forse non sarà preciso, ma di sicuro ci si avvicina!
+	}
 }
 
 int	ft_mouse_manage(int keycode, int x, int y, t_env *e)
 {
 	if (keycode == 4)
-		zoom_plus(x, y, e);
+		ft_zoom(x, y, e, 1);
 	if (keycode == 5)
-		zoom_minus(x, y, e);
+		ft_zoom(x, y, e, 0);
 }
 
 int	ft_command(int keycode, t_env *e)
